6fcd08c12d07e2a0df5cb714d3494154577d4b06
montsecruz9 <montsecruzg9@gmail.com>
2023-03-06 15:20:22 -0600

    adding contract address

--- contract.sol
+++ contract.sol
@@ -5,4 +5,11 @@ import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumerV3 {
 
+  // Create a `public` variable named `priceFeed` of type `AggregatorV3Interface`.
+  AggregatorV3Interface public priceFeed;
+  // Create a constructor
+  constructor() public {
+    // Instantiate the `AggregatorV3Interface` contract
+    priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+  }
 }
