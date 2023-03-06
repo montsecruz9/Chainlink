a173f01e0309f7426566894c404432d1e3655ae4
montsecruz9 <montsecruzg9@gmail.com>
2023-03-06 16:14:56 -0600

    created function

--- contract.sol
+++ contract.sol
@@ -12,4 +12,9 @@ contract PriceConsumerV3 {
     // Instantiate the `AggregatorV3Interface` contract
     priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
   }
+
+  function getLatestPrice() public view returns (int) {
+    (,int price,,,) = priceFeed.latestRoundData();
+    return price;
+  }
 }
