3b1369ae5a1ce94419009bafc663fb08cca98088
montsecruz9 <montsecruzg9@gmail.com>
2023-03-06 14:32:03 -0600

    importing Chainlink contract

--- contract.sol
+++ contract.sol
@@ -1,5 +1,7 @@
-pragma solidity ^0.6.7; 
+pragma solidity ^0.6.7;
 
+// Import Chainlink Data Feeds contract
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsumerV3 {
 
